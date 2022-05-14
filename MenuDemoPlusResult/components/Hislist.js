8945cb0a513e6675bd0769e30df589f912d98cae
long148 <71367287+long148@users.noreply.github.com>
2022-05-14 14:25:29 +0700

    Update Hislist.js

--- MenuDemoPlusResult/components/Hislist.js
+++ MenuDemoPlusResult/components/Hislist.js
@@ -6,38 +6,173 @@ import { AntDesign,Feather,SimpleLineIcons,MaterialCommunityIcons } from '@expo/
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {StackedBarChart,LineChart} from "react-native-chart-kit";
+import { VictoryBar,VictoryChart } from 'victory';
 
 const { width } = Dimensions.get('screen');
 const colors = ['#6190E8', '#A7BFE8', ]
 const Stack = createNativeStackNavigator();
 
-function Item({ item }) {
-  return (
-    <View style={styles.listItem}>
-      <Image source={{uri:item.photo}}  style={{width:60, height:60,borderRadius:30}} />
-      <View style={{alignItems:"center",flex:1}}>
-        <Text style={{fontWeight:"bold"}}>{item.name}</Text>
-        <Text>{item.position}</Text>
-      </View>
-      <TouchableOpacity style={{height:50,width:50, justifyContent:"center",alignItems:"center"}}>
-        <Text style={{color:"green"}}>Call</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
+ 
+
+
 
 const data = [
-  { id: '1', title: 'First item' ,Satus: 'Happy',Date:'00/00/00' },
-  { id: '2', title: 'Second item' ,Satus: 'Happy',Date:'00/00/00' },
-  { id: '3', title: 'Third item' ,Satus: 'Happy' ,Date:'00/00/00' },
-  { id: '4', title: 'Fourth item' ,Satus: 'Happy',Date:'00/00/00'},
-  { id: '5', title: 'Fifth item' ,Satus: 'Happy' ,Date:'00/00/00' },
-  { id: '6', title: 'Sixth item' ,Satus: 'Happy',Date:'00/00/00'},
-  { id: '7', title: 'Seventh item' ,Satus: 'Happy' ,Date:'00/00/00'},
-  { id: '8', title: 'Eight item' ,Satus: 'Happy',Date:'00/00/00'},
-  { id: '9', title: 'Eight item' ,Satus: 'Happy',Date:'00/00/00' },
-  { id: '10', title: 'Eight item',Satus: 'Happy',Date:'00/00/00' },
+  { id: '1', title: 'First item' ,Satus: 'Happy',Date:'00/00/00',},
+  { id: '2', title: 'Second item' ,Satus: 'Happy',Date:'00/00/00', },
+  { id: '3', title: 'Third item' ,Satus: 'Happy' ,Date:'00/00/00', },
+  { id: '4', title: 'Fourth item' ,Satus: 'Happy',Date:'00/00/00',},
+  { id: '5', title: 'Fifth item' ,Satus: 'Happy' ,Date:'00/00/00', },
+  { id: '6', title: 'Sixth item' ,Satus: 'Happy',Date:'00/00/00',},
+  { id: '7', title: 'Seventh item' ,Satus: 'Happy' ,Date:'00/00/00',},
+  { id: '8', title: 'Eight item' ,Satus: 'Happy',Date:'00/00/00',},
+  { id: '9', title: 'Eight item' ,Satus: 'Happy',Date:'00/00/00', },
+  { id: '10', title: 'Eight item',Satus: 'Happy',Date:'00/00/00',},
 ];
+
+
+const MyLineChart = () => {
+  return (
+    <>
+      <Text style={styles.header}>Result</Text>
+      <LineChart
+        data={{
+          labels: ['0:00', '6:00', '9:00', '12:00', '18:00',],
+          legend:['Happy','Sad','Neutral','Supried','Angry','Fear'],
+          datasets: [
+            //Happy : Green #008000
+            {
+              data: [Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                ],
+                color: (opacity = 1) => `#008000 `,
+              strokeWidth: 2,
+            },
+
+             //Sad
+            {
+              data: [Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                ],
+                color: (opacity = 1) => `#ff0000 `,
+              strokeWidth: 2,
+            },
+               //Neutrual
+             {
+              data: [Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                ],
+              color: (opacity = 1) => `#808080`,
+              strokeWidth: 2,
+            },
+               //Suprised
+             {
+              data: [Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                ],
+               color: (opacity = 1) => `#ffff00`,
+              strokeWidth: 2,
+            },
+              //Scared
+             {
+              data: [Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                ],
+                color: (opacity = 1) => `#800080`,
+              strokeWidth: 2,
+            },
+              //Angry
+             {
+              data: [Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+               ],
+              color: (opacity = 1) => `#0000ff`,
+              strokeWidth: 2,
+            },
+
+             {
+              data: [Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                ],
+                color: (opacity = 1) => `#e69138`,
+                
+              strokeWidth: 2,
+            },
+          ],
+        }}
+        
+        width={Dimensions.get('window').width }
+        height={220}
+        chartConfig={{
+          backgroundColor: '#1cc510',
+          backgroundGradientFrom: '#fff',
+          backgroundGradientTo: '#fff',
+          useShadowColorFromDataset:false,
+          decimalPlaces: 2,
+          barPercentage: 0.5,
+          color: (opacity = 1) => `rgba(255,255,255 ${opacity})`,
+          
+
+          style: {
+            borderRadius: 16,
+    
+          },
+        }}
+        
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+          alignItems:'center',
+          
+        }}
+      />
+    </>
+  );
+};
+
+
+
+const getItem = (chart) => {
+ 
+    Alert.alert(chart);
+ 
+  }
+ 
+  const ItemRender = ({ name }) => (
+    <View style={styles.item}>
+      <Text style={styles.itemText} onPress={()=> getItem(name)}>{name}</Text>
+    </View>
+  );
+ 
+ 
+  const getname = (name) => {
+ 
+    Alert.alert(name);
+ 
+  };
+
+  
 
 //MenuScreen//Screen1
 export default function Hislist() {
@@ -63,16 +198,27 @@ export default function Hislist() {
         showsVerticalScrollIndicator={false}
         data={data}
         keyExtractor={item => item.id}
+        
         renderItem={({ item }) => (
           <View style={styles.listItem}>
+            <ItemRender id={item.id} name={item.name}/>
             <Text style={styles.listItemText}>Name:{item.title}</Text>
             <Text style={styles.listItemText}>Date:{item.Date}</Text>
             <Text style={styles.listItemText2}>Satus:{item.Satus}</Text>
-          </View>
-        )}
 
-        
+            <MyLineChart/>
+       
+      
+            
+          </View>
+
+
+        )}
+      
+      
+      
       />
+   
     
         
 
@@ -139,8 +285,12 @@ const styles = StyleSheet.create({
 
   move:{
     margin: 4,
-  }
+  },
 
+  header:{
+    textAlign:'center',
+    margin : 3,
+  }
    
 
 
