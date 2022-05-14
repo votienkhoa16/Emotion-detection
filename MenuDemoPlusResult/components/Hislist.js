e8db7fb4b8adc4c22cf302e8dfecdc96f405e7e0
votienkhoa16 <91238671+votienkhoa16@users.noreply.github.com>
2022-05-14 14:31:32 +0700

    Update Hislist.js

--- MenuDemoPlusResult/components/Hislist.js
+++ MenuDemoPlusResult/components/Hislist.js
@@ -13,27 +13,10 @@ const { width } = Dimensions.get('screen');
 const colors = ['#6190E8', '#A7BFE8', ]
 const Stack = createNativeStackNavigator();
 
- 
 
-
-
-const data = [
-  { id: '1', title: 'First item' ,Satus: 'Happy',Date:'00/00/00',},
-  { id: '2', title: 'Second item' ,Satus: 'Happy',Date:'00/00/00', },
-  { id: '3', title: 'Third item' ,Satus: 'Happy' ,Date:'00/00/00', },
-  { id: '4', title: 'Fourth item' ,Satus: 'Happy',Date:'00/00/00',},
-  { id: '5', title: 'Fifth item' ,Satus: 'Happy' ,Date:'00/00/00', },
-  { id: '6', title: 'Sixth item' ,Satus: 'Happy',Date:'00/00/00',},
-  { id: '7', title: 'Seventh item' ,Satus: 'Happy' ,Date:'00/00/00',},
-  { id: '8', title: 'Eight item' ,Satus: 'Happy',Date:'00/00/00',},
-  { id: '9', title: 'Eight item' ,Satus: 'Happy',Date:'00/00/00', },
-  { id: '10', title: 'Eight item',Satus: 'Happy',Date:'00/00/00',},
-];
-
-
-const MyLineChart = () => {
+const HistoryScreen = () => {
   return (
-    <>
+      
       <Text style={styles.header}>Result</Text>
       <LineChart
         data={{
@@ -133,110 +116,26 @@ const MyLineChart = () => {
           barPercentage: 0.5,
           color: (opacity = 1) => `rgba(255,255,255 ${opacity})`,
           
-
           style: {
             borderRadius: 16,
-    
           },
-        }}
+         }}
         
         style={{
           marginVertical: 8,
           borderRadius: 16,
           alignItems:'center',
-          
         }}
       />
     </>
   );
 };
 
-
-
-const getItem = (chart) => {
- 
-    Alert.alert(chart);
- 
-  }
- 
-  const ItemRender = ({ name }) => (
-    <View style={styles.item}>
-      <Text style={styles.itemText} onPress={()=> getItem(name)}>{name}</Text>
-    </View>
-  );
- 
- 
-  const getname = (name) => {
- 
-    Alert.alert(name);
- 
-  };
-
-  
-
-//MenuScreen//Screen1
-export default function Hislist() {
-    return (
-        <View style={styles.container}>
-            {colors.map((x, i) => (
-                <View style={[styles.bgCircle1, {
-                    backgroundColor: x,
-                    transform: [
-                        { translateX: -(width/6) + (i * width / colors.length) },
-                        { translateY: -(width * 0.6) - (i / 1.25 * width / colors.length) }
-                    ]
-                }]} key={i.toString()} />
-            ))}
-
-
-
-         <Text style={styles.text}></Text> 
-         <Text style={styles.title}>Emotion Detection</Text> 
-       <FlatList
-       vertical
-        style={styles.move}
-        showsVerticalScrollIndicator={false}
-        data={data}
-        keyExtractor={item => item.id}
-        
-        renderItem={({ item }) => (
-          <View style={styles.listItem}>
-            <ItemRender id={item.id} name={item.name}/>
-            <Text style={styles.listItemText}>Name:{item.title}</Text>
-            <Text style={styles.listItemText}>Date:{item.Date}</Text>
-            <Text style={styles.listItemText2}>Satus:{item.Satus}</Text>
-
-            <MyLineChart/>
-       
-      
-            
-          </View>
-
-
-        )}
-      
-      
-      
-      />
-   
-    
-        
-
-        </View>
-    )
-}
-
-
-
-
-
-
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
+       flex: 1,
+       alignItems: 'center',
         
-       
     },
     bgCircle1: {
         position: 'absolute',
@@ -247,12 +146,9 @@ const styles = StyleSheet.create({
         top: 0
     },
     
-
- 
   text: {
     marginBottom:190,
   },
-
 
   title: {
     bottom: 100,
@@ -267,20 +163,17 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor:'#fff',
     width: 300,
-    borderRadius:16,
-    
+    borderRadius:16, 
   },
   
   listItemText: {
     fontSize: 16,
-    
   },
-
+ 
   listItemText2: {
     fontSize: 14,
     textAlign : "right",
     bottom: 15,
-    
   },
 
   move:{
