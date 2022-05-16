91f00df975716fcf2fb594d4ecd69e3e81a4ab75
long148 <71367287+long148@users.noreply.github.com>
2022-05-16 16:06:03 +0700

    Update info.js

--- MenuDemoPlusResult/components/info.js
+++ MenuDemoPlusResult/components/info.js
@@ -48,10 +48,8 @@ export default function InfoDetailList() {
         >
           <View style={styles.textContainer}>
             <Text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              quis erat suscipit, mollis nibh ut, venenatis lectus. Orci varius
-              natoque penatibus et magnis dis parturient montes, nascetur
-              ridiculus mus.
+              Welcome User to The Emotion Detection where you can be detect your emotion result with your phone as it's Fast and Simple to use.
+              In order to use the App , You must have an account to use . If you don't have an Account , Please Sign Up for an account in order to use it
             </Text>
           </View>
         </AnimatedSection>
@@ -72,10 +70,9 @@ export default function InfoDetailList() {
         >
           <View style={styles.textContainer}>
             <Text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              quis erat suscipit, mollis nibh ut, venenatis lectus. Orci varius
-              natoque penatibus et magnis dis parturient montes, nascetur
-              ridiculus mus.
+             To dectect your current emotion that you are having right now ,you must click on the camera icon to take a picture of a person face or a selfie.
+             Then after taking the photo, it will scan the result the result and will display the result with the demonstariton chart.
+             In case of a person is currently Sad ,there will be a section that you can send to your guardian to know your mood by type the input of their email
             </Text>
           </View>
         </AnimatedSection>
@@ -96,10 +93,8 @@ export default function InfoDetailList() {
         >
           <View style={styles.textContainer}>
             <Text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              quis erat suscipit, mollis nibh ut, venenatis lectus. Orci varius
-              natoque penatibus et magnis dis parturient montes, nascetur
-              ridiculus mus.
+              The Result will display the both on chart and the status of the emotion that you are on.
+              If the current status that you are Sad, there will be a section that you can send to your guardian to know your mood by type the input of their email
             </Text>
           </View>
         </AnimatedSection>
@@ -121,10 +116,7 @@ export default function InfoDetailList() {
         >
           <View style={styles.textContainer}>
             <Text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              quis erat suscipit, mollis nibh ut, venenatis lectus. Orci varius
-              natoque penatibus et magnis dis parturient montes, nascetur
-              ridiculus mus.
+              The history will display of user that using the app which are the last status, date of using the name of the user . With the addition of Multiple Chart 
             </Text>
           </View>
         </AnimatedSection>
